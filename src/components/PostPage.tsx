738d03f261312dbd6a05836b85c01e6557e321b9
mikhita <mikheilispiriani@yahoo.com>
2023-01-19 13:32:04 +0400

    I get users by posts userIds

--- src/components/PostPage.tsx
+++ src/components/PostPage.tsx
@@ -5,7 +5,14 @@ import { useParams } from 'react-router-dom';
   
   const PostPage = () => {
     const [post, setPost] = useState<any>({});
+    const [user, setUser] = useState<any>({});
+    const [comment, setComment] = useState<any>({});
+
     const params = useParams();
+    const paramsUser = useParams();
+
+    console.log(paramsUser)
+
     
 
     useEffect(() => {
@@ -14,13 +21,33 @@ import { useParams } from 'react-router-dom';
         .then(response => response.json())
         .then(data => setPost(data))
     }, [params.id])
-    console.log(post)
+    
+    useEffect(() => {
+        // fetch the post with the user id
+        fetch(`https://jsonplaceholder.typicode.com/users/${paramsUser.userId}`)
+          .then(response => response.json())
+          .then(data => setUser(data))
+      }, [paramsUser.userId])
+    //   console.log(post.userId)
+      
 
 
   return (
     <div>
-      <h1>{post.title}</h1>
-      <p>{post.body}</p>
+      <ul>
+        <li>
+            <h1>{"post title: "}{post.title}</h1>
+            <p>{"post content: "}{post.body}</p>
+        </li>
+        <li>
+            <h1>{"user name and last name: "}{user.name} </h1>
+            <h2>{"user email: "}{user.email}</h2>
+        </li>
+        <li>
+            <h1>{"comments: "}</h1>
+
+        </li>
+      </ul>
     </div>
      )  ;
   
